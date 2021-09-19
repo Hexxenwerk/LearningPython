c1379e38a22169984386b183e146f71df3ff9230
Björn Kötter <dev@bk.netburn.de>
2021-09-19 14:31:17 +0200

    relativen Abstand zur Ratezahl hinzugefügt

--- zahlenraten.py
+++ zahlenraten.py
@@ -7,13 +7,20 @@ if not isPlay:
     exit(0)
 
 name: str = input("Super. Viel Spaß. Wie ist dein Name? ")
-zufallszahl: int = random.randint(1, 100)
+bereich = {min: 1, max: 100}
+zufallszahl: int = random.randint(bereich[min], bereich[max])
 eingabe: int = 0
 
 while zufallszahl != eingabe:
-    eingabe: int = int(input(f"{name}, gib eine Zahl zwischen 1 und 100 ein: "))
+    eingabe: int = int(input(f"{name}, gib eine Zahl zwischen {bereich[min]} und {bereich[max]} ein: "))
+    if zufallszahl > eingabe + bereich[max] * 0.2:
+        print("Deine Eingabe war viel zu niedrig. Versuche es noch mal")
+        continue
     if zufallszahl > eingabe:
         print("Deine Eingabe war zu niedrig. Versuch es noch mal.")
+    elif zufallszahl < eingabe - bereich[max] * 0.2:
+        print("Deine Eingabe war viel zu hoch. Versuch es noch mal.")
+        continue
     elif zufallszahl < eingabe:
         print("Deine Eingabe war zu hoch. Versuch es noch mal.")
 
