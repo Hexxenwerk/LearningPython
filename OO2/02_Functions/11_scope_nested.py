12b767ac2852faa51f0468a406ce088029dc5298
Björn Kötter <dev@bk.netburn.de>
2021-09-25 15:47:57 +0200

    Initial commit

---
+++ OO2/02_Functions/11_scope_nested.py
@@ -0,0 +1,21 @@
+x: int = 5
+y: int = 100
+
+
+def outer():
+    x: int = 10
+
+    def inner():
+        nonlocal x
+        global y
+        x = 20
+        y = 200
+        print(f"inner x: {x}")
+
+    inner()
+    print(f"outer x: {x}")
+
+print(f"global x: {x}")
+outer()
+print(f"global x: {x}")
+print(f"y: {y}")
