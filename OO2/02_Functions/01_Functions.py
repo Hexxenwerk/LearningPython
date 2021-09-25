9df7c070607d92bb38e4afa8aca641ae03e802fa
Björn Kötter <dev@bk.netburn.de>
2021-09-25 11:00:24 +0200

    Improve functions, eliminate global variables

--- OO2/02_Functions/01_Functions.py
+++ OO2/02_Functions/01_Functions.py
@@ -1,7 +1,22 @@
-def greeting(name: str):
+def greeting(name: str, years: int = 0):
     print(f'Hello {name}')
-    print('Welchen Erfahrungshintergrund hast du?')
-    print('Warum Python Kurs 2?')
+    print(f'Du hast {years} Hintergrund in Softwareentwicklung.')
+    print('Warum besuchst du Python Kurs 2?')
 
 
-greeting("Ingo")
+def get_background_years():
+    try:
+        years: int = int(input("Wie viele Jahre programmierst du schon? "))
+        return years
+    except ValueError:
+        print("Die Eingabe muss numerisch sein.")
+        return get_background_years()
+
+
+def main():
+    name: str = input("Wie ist dein Name? ")
+    years = get_background_years()
+    greeting(name, years)
+
+
+exit(main())
