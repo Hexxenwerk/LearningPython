3bd3c36a3aa5c7abc53b2148821f4e30f7dec0c2
Björn Kötter <dev@bk.netburn.de>
2021-09-26 10:58:13 +0200

    explicit variable type declaration

--- OO2/04_Project/app.py
+++ OO2/04_Project/app.py
@@ -5,8 +5,8 @@ import translate
 def main() -> int:
     path: str = input("Gib den Pfad ein, der nach Dateien durchsucht werden soll ['.']: ")
     selector: str = input("Gib einen Selektor an oder Enter für [*.txt]: ")
-    line_count = int(input("Gib die Anzahl der zu übersetzenden Zeilen ein: "))
-    file_list = files.get_files_in_dir(path, selector)
+    line_count: int = int(input("Gib die Anzahl der zu übersetzenden Zeilen ein: "))
+    file_list: list = files.get_files_in_dir(path, selector)
 
     if len(file_list) < 1:
         print(f"No files found under {path} for {selector}")
