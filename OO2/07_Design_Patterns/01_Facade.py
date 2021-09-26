df4fd797757db47bfd3cc91dbc3cb19d32fb681a
Björn Kötter <dev@bk.netburn.de>
2021-09-26 17:44:35 +0200

    Initial commit

---
+++ OO2/07_Design_Patterns/01_Facade.py
@@ -0,0 +1,16 @@
+class CourseBooking:
+    def __init__(self) -> None:
+        pass
+
+
+class CoursePayment:
+    def __init__(self) -> None:
+        pass
+
+
+def app_web():
+    pass
+
+
+def app_mobile():
+    pass
--- OO2/playground.py
+++ OO2/playground.py
@@ -1,10 +1,18 @@
-class Person:
-    def __init__(self, name: str):
-        self.name = name
+def decorate_message(fun):
+    # Nested function
+    def add_welcome(site_name) -> str:
+        return "Welcome to " + fun(site_name)
 
-    def talk(self, say: str):
-        print(f"{self.name} says {say}")
+    # Decorator returns a function
+    return add_welcome
 
 
-p1 = Person("Suzi")
-p1.talk("Hello")
+@decorate_message
+def site(site_name: str) -> str:
+    return site_name
+
+
+# This call is equivalent to call to
+# decorate_message() with function
+# site("GeeksforGeeks") as parameter
+print(site("GeeksforGeeks"))
