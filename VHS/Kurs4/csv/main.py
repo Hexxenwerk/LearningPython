f9a94a97d915898fa185446240c3a93623ec2c33
Björn Kötter <dev@bk.netburn.de>
2022-01-30 17:38:17 +0100

    funktioniert noch nicht

--- VHS/Kurs4/csv/main.py
+++ VHS/Kurs4/csv/main.py
@@ -5,8 +5,10 @@ import requests
 
 def load_data():
     response = requests.get('https://support.staffbase.com/hc/en-us/article_attachments/360009197031/username.csv')
-    csv_result = csv.reader(response.text)
-    print(type(csv_result))
+    csv_reader = csv.reader(response.text, delimiter=';')
+    for row in csv_reader:
+        print(''.join(row), end='')
+    print(response.text)
 
 
 def main():
