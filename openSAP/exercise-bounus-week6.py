c6bc270486d827daec1bfb7e5d3c7fea6c8560ed
Björn Kötter <dev@bk.netburn.de>
2022-05-21 15:39:27 +0200

    optimize, remove array

--- openSAP/exercise-bounus-week6.py
+++ openSAP/exercise-bounus-week6.py
@@ -2,8 +2,8 @@ import math
 import random
 
 points_in_square = [(random.random(), random.random()) for _ in range(10000)]
-points_in_circle = [(x, y) for x, y in points_in_square if x ** 2 + y ** 2 < 1]
-my_pi = len(points_in_circle) / len(points_in_square) * 4
+count_in_circle = len([(x, y) for x, y in points_in_square if x * x + y * y < 1])
+my_pi = count_in_circle / len(points_in_square) * 4
 
 print(f'Calculated value of π: {my_pi}')
 
