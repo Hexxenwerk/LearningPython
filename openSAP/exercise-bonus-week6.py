5f2996f3da6e1399d63591b6c6d8e7139ce76577
Björn Kötter <dev@bk.netburn.de>
2022-05-24 20:29:58 +0200

    optimize code

--- openSAP/exercise-bonus-week6.py
+++ openSAP/exercise-bonus-week6.py
@@ -1,11 +1,9 @@
-import math
-import random
+from math import pi
+from random import random
 
-points_in_square = [(random.random(), random.random()) for _ in range(10000)]
-count_in_circle = len([(x, y) for x, y in points_in_square if x * x + y * y < 1])
-my_pi = count_in_circle / len(points_in_square) * 4
+my_pi = len([True for _ in range(10000) if random() ** 2 + random() ** 2 < 1]) / 10000 * 4
 
 print(f'Calculated value of π: {my_pi}')
 
-print(f'Value of π from math library: {math.pi}')
-print(f'Difference: {abs(math.pi - my_pi)}')
+print(f'Value of π from math library: {pi}')
+print(f'Difference: {abs(pi - my_pi)}')
